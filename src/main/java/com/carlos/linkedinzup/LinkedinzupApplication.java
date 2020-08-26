4309e936fae582d2c577ab7bb2f7136ffc4ea3fd
Carlos <carlosdudujunior@hotmail.com>
2020-08-26 19:30:58 -0300

    Inicializando projeto

---
+++ src/main/java/com/carlos/linkedinzup/LinkedinzupApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.linkedinzup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinzupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkedinzupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carlos/linkedinzup/LinkedinzupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.linkedinzup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinzupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
