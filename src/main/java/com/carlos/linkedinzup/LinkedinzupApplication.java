b0b5376f2de664c5f5daa686bd3b77bcd3b9bc54
Carlos <carlosdudujunior@hotmail.com>
2020-08-28 02:07:25 -0300

    Scaneando base de pacotes da model

--- src/main/java/com/carlos/linkedinzup/LinkedinzupApplication.java
+++ src/main/java/com/carlos/linkedinzup/LinkedinzupApplication.java
@@ -2,8 +2,10 @@ package com.carlos.linkedinzup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.carlos.linkedinzup.model")
 public class LinkedinzupApplication {
 
     public static void main(String[] args) {
