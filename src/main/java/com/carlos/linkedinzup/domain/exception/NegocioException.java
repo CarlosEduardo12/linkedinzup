4becca393b7752a009ca2868c3d4844cfa0a096b
Carlos <carlosdudujunior@hotmail.com>
2020-08-30 01:42:22 -0300

    adicionado serialVersionUID

--- src/main/java/com/carlos/linkedinzup/domain/exception/NegocioException.java
+++ src/main/java/com/carlos/linkedinzup/domain/exception/NegocioException.java
@@ -1,6 +1,8 @@
 package com.carlos.linkedinzup.domain.exception;
 
 public class NegocioException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public NegocioException(String message){
         super(message);
     }
