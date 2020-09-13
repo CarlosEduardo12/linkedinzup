678b1b68c3479550b6c4f63c370991087d4c223d
Carlos <carlosdudujunior@hotmail.com>
2020-09-13 14:37:26 -0300

    Mudando retorno do método para Optional

--- src/main/java/com/carlos/linkedinzup/domain/repository/VagaRepository.java
+++ src/main/java/com/carlos/linkedinzup/domain/repository/VagaRepository.java
@@ -6,11 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface VagaRepository  extends JpaRepository<Vaga, Long> {
     @Query(value = "SELECT * FROM Vagas WHERE cargo = ?1 AND empresa_Id = ?2", nativeQuery = true)
-    Optional<Vaga> findByCargoAndEmpresa(String cargo, Long id);
+    Vaga findByCargoAndEmpresa(String cargo, Long id);
 
 }
